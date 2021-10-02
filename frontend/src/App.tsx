3ecec71a3896084ee1840f151c9ae257b761344c
Wanasit Tanakitrungruang <wanasit.wt@gmail.com>
2021-10-02 11:46:46 +0900

    Fix: SCSS usage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
-import './App.scss';
 import styled from "styled-components";
 import {HomePage} from "./pages/HomePage"
 import {Nav, Navbar, Container} from "react-bootstrap";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const AppContainer = styled.div`
   padding: 4em 0em;
